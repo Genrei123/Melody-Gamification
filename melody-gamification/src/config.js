9f1dd09c9d8817f327c3bd97114f4aef03730191
SKY <listangco21@GMAIL.COM>
2024-12-17 22:32:16 +0800

    Create config.js

---
+++ melody-gamification/src/config.js
@@ -0,0 +1,5 @@
+// src/config.js
+export const BACKEND_URLS = {
+    saveComposition: 'http://localhost/melody-backend/save_compositions.php',
+    fetchCompositions: 'http://localhost/melody-backend/fetch_compositions.php'
+  };
